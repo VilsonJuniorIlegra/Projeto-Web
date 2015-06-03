e6a7185f89a24222a280e12236d2d5a12cb9fc9c
VilsonJuniorIlegra <vilson.junior@ilegra.com>
2015-06-03 10:09:03 -0300

    criado estrutura do projeto

---
+++ Desafio3/src/test/java/desafio3/Desafio3ApplicationTests.java
@@ -0,0 +1,18 @@
+package desafio3;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Desafio3Application.class)
+@WebAppConfiguration
+public class Desafio3ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
